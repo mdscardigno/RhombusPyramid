1096d0598d3d6ae1b8dbede14b34637d4fb41334
Milagro Scardigno <mdscardigno@gmail.com>
2021-11-04 14:19:05 -0400

    Creating nested loop to print asterisks

--- Program.cs
+++ Program.cs
@@ -2,11 +2,32 @@
 
 namespace RhombusPyramid
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Rhombus Pyramid");
+            //Write a program that asks an user to enter a whole number and draws a rhombus pyramid. For example: 
+            //If the user enters 6 numbers the output will be as follows:
+            /*
+            *
+            **
+            ***
+            ****
+            *****
+            ******
+            */
+
+            Console.WriteLine("Please enter a number and I will paint a pyramid:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int counter = 1; counter <= number; counter++)
+            {
+                for (int asterisk = 1; asterisk <= counter; asterisk++)
+                {
+                    Console.WriteLine("*");
+                }
+            }
+        }
     }
-  }
 }
